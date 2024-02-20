4e080ea76eac41931d3ee8df79492a641245198d
elias <eliascamilo70@outlook.com>
2024-02-20 13:59:01 -0300

    java project - controller,service

--- src/main/java/com/elias/demoparkapi/service/UsuarioService.java
+++ src/main/java/com/elias/demoparkapi/service/UsuarioService.java
@@ -1,6 +1,8 @@
 package com.elias.demoparkapi.service;
 
+import com.elias.demoparkapi.entity.Usuario;
 import com.elias.demoparkapi.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
@@ -8,5 +10,10 @@ import org.springframework.stereotype.Service;
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+    @Transactional
+    public Usuario salvar(Usuario usuario) {
+        return usuarioRepository.save(usuario);
 
+
+    }
 }
